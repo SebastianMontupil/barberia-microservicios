834f26324c4fecea7a8ec4fdfd33ff52af662608
VicenteMartinez <vichodelbulla06@gmail.com>
2026-05-26 19:57:31 -0300

    Se agrega manejo de errores en el microservicio agendas

---
+++ agendas/src/main/java/com/barberia/agendas/dto/ErrorDTO.java
@@ -0,0 +1,16 @@
+package com.barberia.agendas.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+
+    private LocalDateTime fecha;
+    private Integer estado;
+    private String error;
+    private String mensaje;
+}
