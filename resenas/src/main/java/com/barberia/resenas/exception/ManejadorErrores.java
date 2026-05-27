d7296a43892113583732bd84547d374a2cc908d4
SebastianMontupil <tatamoncar@gmail.com>
2026-05-26 20:48:16 -0400

    Se agrega manejo de errores en el microservicio resenas

---
+++ resenas/src/main/java/com/barberia/resenas/exception/ManejadorErrores.java
@@ -0,0 +1,23 @@
+package com.barberia.resenas.exception;
+
+import com.barberia.resenas.dto.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ManejadorErrores {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO manejarRuntimeException(RuntimeException ex) {
+
+        return new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                "Error en la solicitud",
+                ex.getMessage()
+        );
+    }
+}
