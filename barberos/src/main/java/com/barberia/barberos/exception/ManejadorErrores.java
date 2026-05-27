de137d3354473ce67f6f668ef117b80eaf3a136e
SebastianMontupil <tatamoncar@gmail.com>
2026-05-26 20:11:55 -0400

    Se agrega manejo de errores en el microservicio barberos

---
+++ barberos/src/main/java/com/barberia/barberos/exception/ManejadorErrores.java
@@ -0,0 +1,23 @@
+package com.barberia.barberos.exception;
+
+import com.barberia.barberos.dto.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ManejadorErrores {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO manejarRuntimeException(RuntimeException ex) {
+
+        return new ErrorDTO(
+                LocalDateTime.now(),
+                400,
+                "Error en la solicitud",
+                ex.getMessage()
+        );
+    }
+}
